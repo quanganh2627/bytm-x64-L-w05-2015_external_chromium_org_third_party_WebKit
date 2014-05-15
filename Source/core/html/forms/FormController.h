f523d2789ac2f83c4eca0ee4d5161bfdb5f2d052
Torne (Richard Coles) <torne@google.com>
2014-05-15 12:16:45 +0100

    Merge from Chromium at DEPS revision 269467
    This commit was generated by merge_to_master.py.
    
    Change-Id: Ibe4bf25b1e1d66aba1e13e7c79b3c608dc0db399

--- Source/core/html/forms/FormController.h
+++ Source/core/html/forms/FormController.h
@@ -74,10 +74,10 @@ inline void FormControlState::append(const String& value)
 
 typedef HashMap<AtomicString, OwnPtr<SavedFormState> > SavedFormStateMap;
 
-class DocumentState : public RefCountedWillBeGarbageCollectedFinalized<DocumentState> {
+class DocumentState FINAL : public RefCountedWillBeGarbageCollected<DocumentState> {
+    DECLARE_EMPTY_DESTRUCTOR_WILL_BE_REMOVED(DocumentState);
 public:
     static PassRefPtrWillBeRawPtr<DocumentState> create();
-    ~DocumentState();
     void trace(Visitor*);
 
     void addControl(HTMLFormControlElementWithState*);
@@ -89,7 +89,7 @@ private:
     FormElementListHashSet m_formControls;
 };
 
-class FormController : public NoBaseWillBeGarbageCollectedFinalized<FormController> {
+class FormController FINAL : public NoBaseWillBeGarbageCollectedFinalized<FormController> {
     WTF_MAKE_FAST_ALLOCATED_WILL_BE_REMOVED;
 public:
     static PassOwnPtrWillBeRawPtr<FormController> create()
@@ -121,7 +121,7 @@ private:
     RadioButtonGroupScope m_radioButtonGroupScope;
     RefPtrWillBeMember<DocumentState> m_documentState;
     SavedFormStateMap m_savedFormStateMap;
-    OwnPtr<FormKeyGenerator> m_formKeyGenerator;
+    OwnPtrWillBeMember<FormKeyGenerator> m_formKeyGenerator;
 };
 
 } // namespace WebCore
